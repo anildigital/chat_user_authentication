b01c862668afb53e0ab27b96108ee92f2058a115
Anil Wadghule <anildigital@gmail.com>
2020-01-02 17:32:55 +0530

    Add tests for users create, update

--- lib/chat/users.ex
+++ lib/chat/users.ex
@@ -1,2 +1,33 @@
 defmodule Chat.Users do
+  alias Chat.Users.User
+
+  alias Chat.Repo
+
+  def create(params) do
+    changeset = User.create_changeset(%User{}, params)
+
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        user
+        #        |> Emails.welcome_email()
+        #        |> Mailer.deliver_later()
+
+        {:ok, user}
+
+      {:error, changeset} ->
+        {:error, changeset}
+    end
+  end
+
+  def update(user, params) do
+    changeset = User.update_changeset(user, params)
+
+    case Repo.update(changeset) do
+      {:ok, user} ->
+        {:ok, user}
+
+      {:error, changeset} ->
+        {:error, changeset}
+    end
+  end
 end
--- test/chat/users_test.exs
+++ test/chat/users_test.exs
@@ -2,12 +2,13 @@ defmodule Chat.UsersTest do
   use Chat.DataCase
 
   alias Chat.Users
+  alias Chat.TestHelpers
 
   describe "Creating new users" do
     test "registers them" do
       {:ok, user} =
         Users.create(%{
-          email: "use@example.com",
+          email: "user@example.com",
           first_name: "John",
           last_name: "User",
           password: "password",
@@ -17,6 +18,22 @@ defmodule Chat.UsersTest do
       assert user.email == "user@example.com"
       assert user.first_name == "John"
       assert(user.last_name == "User")
+    end
+  end
+
+  describe "Updating users" do
+    test "change basic info" do
+      {:ok, user} = TestHelpers.create_user()
+
+      {:ok, user} =
+        Users.update(user, %{
+          first_name: "Updated",
+          last_name: "Last name"
+        })
+
+      assert user.first_name == "Updated"
+
+      assert user.last_name == "Last name"
     end
   end
 end
