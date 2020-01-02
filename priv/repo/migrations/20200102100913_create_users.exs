74f878f31d93ae069a7750fa01c19d3b0a1dd702
Anil Wadghule <anildigital@gmail.com>
2020-01-02 17:32:25 +0530

    Add tests for user create, update

---
+++ priv/repo/migrations/20200102100913_create_users.exs
@@ -0,0 +1,24 @@
+defmodule Chat.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add(:email, :string)
+      add(:first_name, :string)
+      add(:last_name, :string)
+      add(:password, :string)
+      add(:password_hash, :string)
+
+      add(:token, :string)
+      add(:email_verification_token, :uuid)
+      add(:email_verified_at, :utc_datetime)
+
+      add(:password_reset_token, :uuid)
+      add(:password_reset_expires_at, :utc_datetime)
+
+      timestamps()
+    end
+
+    create(index(:users, ["lower(email)"], unique: true))
+  end
+end
